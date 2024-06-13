d0cabeaa8323a3cd67a1fc9e62f753508c7923b0
arko <arvinakori7@gmail.com>
2024-06-13 14:06:38 +0530

    Convex Demo project

--- convex/messages.ts
+++ convex/messages.ts
@@ -6,8 +6,24 @@ export const list = query({
   handler: async (ctx) => {
     // Grab the most recent messages.
     const messages = await ctx.db.query("messages").order("desc").take(100);
+    const messagesWithLikes = await Promise.all(
+      messages.map(async (message) => {
+        const likes = await ctx.db
+          .query("likes")
+          .withIndex("byMessageId", (q) => q.eq("messageId", message._id))
+          .collect();
+
+        return {
+          ...message,
+          likes: likes.length,
+        };
+      })
+    );
     // Reverse the list so that it's in a chronological order.
-    return messages.reverse();
+    return messagesWithLikes.reverse().map((message) => ({
+      ...message,
+      body: message.body.replaceAll(":)", "😊")
+    }));
   },
 });
 
@@ -16,5 +32,15 @@ export const send = mutation({
   handler: async (ctx, { body, author }) => {
     // Send a new message.
     await ctx.db.insert("messages", { body, author });
+  },
+});
+
+export const like = mutation({
+  args: { liker: v.string(), messageId: v.id("messages") },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("likes", {
+      liker: args.liker,
+      messageId: args.messageId,
+    });
   },
 });
